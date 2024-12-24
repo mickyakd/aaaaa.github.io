4e6e2d4140dbe0b62aa5ec89907295db35daf20c
mickyakd <153686977+mickyakd@users.noreply.github.com>
2024-12-24 00:36:59 -0500

    Add files via upload

---
+++ messages.js
@@ -0,0 +1,6 @@
+const anniversaryDate = 'June 15, 2023 00:00:00' 
+const noMessage = 'Buenos días, mi amor. El propósito de esta página es escribirte mensajes diarios recordándote lo mucho que te adoro.<br>Hoy quiero hablarte sobre lo mucho que me importas.<br>Eres la persona más maravillosa del universo y estoy increíblemente agradecida con él por haberme permitido encontrarte.<br>Valoro mucho cada cosa de ti, eres perfecta, eres el alma más bonita y gentil que existe.<br>Te amo tanto, mi vida, eso no cambiará jamás.'
+const endMessage = '<br><br><br>Con amor,<br>Mickaellita'
+const messages = {
+    '10:11': 'Sample message'
+}
