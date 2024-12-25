81f97319c4591ce281f545f623b3ecaa4f3caaca
mickaellita <153686977+mickyakd@users.noreply.github.com>
2024-12-25 07:40:31 -0500

    Delete messages.js

--- messages.js
+++
@@ -1,6 +0,0 @@
-const anniversaryDate = 'June 15, 2023 00:00:00' 
-const noMessage = '<br>Buenos días mi amor,<br><br>Feliz Navidad! Eres lo mejor que la vida me pudo dar, te adoro con todo mi ser.<br>Mi vida es muy bonita contigo en ella, haces que todo sea mejor siempre porque realmente eres un ser de luz.<br><br>Te amo muchísimo, mi corazón. Eres increíble.'
-const endMessage = '<br><br><br>Con amor,<br>Mickaellita'
-const messages = {
-    '10:11': 'Sample message'
-}
