c5b287057bbb89670fa119541f518b729f219006
mickaellita <153686977+mickyakd@users.noreply.github.com>
2024-12-25 08:10:13 -0500

    Add files via upload

---
+++ messages.js
@@ -0,0 +1,6 @@
+const anniversaryDate = 'June 15, 2023 00:00:00' 
+const noMessage = '<br>Buenos días mi amor,<br><br>Feliz Navidad! Eres lo mejor que la vida me pudo dar, te adoro con todo mi ser.<br>Mi vida es muy bonita contigo en ella, haces que todo sea mejor siempre porque realmente eres un ser de luz.<br><br>Te amo muchísimo, mi corazón. Eres increíble.<br><br>Haz click aquí para una sorpresa: https://www.canva.com/design/DAGaTGhBWIc/5MGAgpZUeSSkszU6AERhrg/edit?utm_content=DAGaTGhBWIc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'
+const endMessage = '<br><br><br>Con amor,<br>Mickaellita'
+const messages = {
+    '10:11': 'Sample message'
+}
