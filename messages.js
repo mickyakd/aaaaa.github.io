50738216298e364f1f8df432eae69abd20473816
mickaellita <153686977+mickyakd@users.noreply.github.com>
2024-12-26 09:49:27 -0500

    Add files via upload

---
+++ messages.js
@@ -0,0 +1,6 @@
+const anniversaryDate = 'June 15, 2023 00:00:00' 
+const noMessage = '<br>Buenos días hermosa,<br><br>Hoy, como es costumbre, soñé contigo.<br>Me gusta saber que estás en mi mente cuando voluntariamente pienso en ti, y cuando lo hago como producto de mi subconsciente.<br>Estoy muy enamorada de ti, amo sentir todo lo que siento por ti, me emociona poder hacerlo por el resto de mi vida.<br>Te adoro muchísimo, mi vida, eres el ser humano más bello del universo.<br><br>Esta es la canción de hoy: https://open.spotify.com/track/1sJFu2pvYJhtEOo9oVe1TZ?si=def8fb5596a54dad'
+const endMessage = '<br><br><br>Con amor,<br>Mickaellita'
+const messages = {
+    '10:11': 'Sample message'
+}
